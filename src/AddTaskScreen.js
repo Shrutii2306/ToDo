cfe1318fc71f0b369546031707052c6d52b37d7b
Shruti Singh <sshrutissingh2002@@gmail.com>
2024-01-30 03:31:01 +0530

    add function created

--- src/AddTaskScreen.js
+++ src/AddTaskScreen.js
@@ -1,21 +1,56 @@
 import React, { useState } from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
+import tempData from './tempData';
+import firestore from '@react-native-firebase/firestore';
 
 const AddTaskScreen = () => {
 
-    const [task,setTask] = useState({});
+    const addTasks = async() => {
 
-    const onTitleChange = (e) => {
+        try{   
+            const docId = await firestore().collection('tasks').add({
+                title: title,
+                description: desc,
+                isComplete: false
+            })
+
+            console.log('task created with id :',docId);
+        }catch(err){
+
+            console.log(err);
+        }
 
     }
+    const fetchTasks = async() => {
 
-    const onDescChange = (e) =>{
+        const tasksCollection = await firestore().collection('tasks').get();
+        
+        tasksCollection.forEach((task) => {
 
+            console.log(task.id);
+        })
+    //    await db.collection("tasks").get().then((querySnapshot) => {
+    //         querySnapshot.forEach((doc) => {
+    //             // doc.data() is never undefined for query doc snapshots
+    //             console.log(doc.id, " => ", doc.data());
+    //         });
+    //     });
     }
+    const [title,setTitle] = useState('');
+    const  [desc, setDesc] = useState('');
 
+    const onAddTask = () =>{
+
+        tempData.push({title:title, description:desc,isComplete:false});
+        console.log(tempData);
+    }
     return (
         <View>
+
             <Text>Add Task</Text>
+            <TextInput placeholder='Enter Title' value={title} onChangeText={(title) => setTitle(title)}/>
+            <TextInput placeholder='Enter Description' value={desc} onChangeText={(desc) => setDesc(desc)}/>
+            <Button title='Add Task' onPress={addTasks}/>
         </View>
     );
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, StyleSheet, Text, TextInput, Button, FlatList, TouchableOpacity} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 //  import { Input, Icon } from '@rneui/themed';
 import tempData from './tempData';
 const Home = ({navigation}) => {
 
-    const call = async() => {  
-          const tasksCollection = await firestore().collection('tasks') .doc('dURJdZAFJjkZoX1hR6Yz')
-          .delete();
-          console.log(tasksCollection)
-    }  
+    const [allTasks,setAllTasks] = useState({});
 
+    const fetchTasks = async() => {
+
+        try{
+
+            let tasks = [];
+            const tasksCollection = await firestore().collection('tasks').get();            
+            tasksCollection.forEach((task) => {
+                console.log(task.id,task.data());
+                tasks.push(task.data());
+
+            })
+            setAllTasks(tasks);
+            console.log(tasks);
+        }catch(err){
+            console.log(err)
+        }
+    //    await db.collection("tasks").get().then((querySnapshot) => {
+    //         querySnapshot.forEach((doc) => {
+    //             // doc.data() is never undefined for query doc snapshots
+    //             console.log(doc.id, " => ", doc.data());
+    //         });
+    //     });
+    }
+
+    useEffect(() => {
+
+        fetchTasks();
+
+    },[])
     return (
         <View>
             
@@ -26,6 +51,7 @@ const Home = ({navigation}) => {
             />
             <TextInput placeholder='Enter a Task..'/>
             <Button title='get' onPress={() => navigation.navigate('AddTask')}/>
+            <Button title='fetch' onPress={fetchTasks}/>
             <Text>ToDo App</Text>
         </View>
     );
